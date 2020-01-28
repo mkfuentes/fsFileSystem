d34e437864551bb3d5b0fa1651862f8d85db2b20
mk fuentes <miguelangel.fm@outlook.com>
2020-01-28 13:47:36 -0600

    ReadMod

--- Read/main.js
+++ Read/main.js
@@ -1,7 +1,5 @@
-var fs = require('fs')
-let contenido
-fs.readFile('index.html', 'utf8', function read(err, data) {
-    if (err) {throw err}
-    contenido = data
-})
-console.log(contenido)
+var fs = require('fs');
+  fs.readFile('index.html', 'utf8' function(err, data) {
+    if (err) throw err
+    console.log(data)
+  });
