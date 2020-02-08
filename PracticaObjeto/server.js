964f6f118ca55546eb7cc2d281aef887c3b92125
mk fuentes <miguelangel.fm@outlook.com>
2020-02-08 15:50:56 -0600

    Cambios

--- PracticaObjeto/server.js
+++ PracticaObjeto/server.js
@@ -16,9 +16,22 @@ const koders = [
   },
 ]
 
+app.get('/koders/name/:name', (request, response) => {
+  console.log('Koders: ', koders)
+  let name = request.params.name
+  response.json(`Este es: ${name}`)
+})
 
+app.get('/koders/id/:id', (request, response) => {
+  console.log('Koders', koders+request.params.id)
+  response.json(`Este es: ${koders[id]}`)
+})
 
 app.get('/koders', (request, response) => {
   console.log('Koders', koders)
-  response.send(`Estos son: ${koders}`)
+  response.json(`Estos son: ${JSON.stringify(koders)}`)
+})
+
+app.listen(8080, () => {
+  console.log('App listening')
 })
