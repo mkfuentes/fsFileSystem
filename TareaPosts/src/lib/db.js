9a804944ccf48d1cdfef1434aec9db4fc3b32b71
mkfuentes <miguelangel.fm@outlook.com>
2020-02-11 17:04:51 -0600

    postTarea

---
+++ TareaPosts/src/lib/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const {
+  DB_PASSWORD,
+  DB_NAME,
+  DB_USER,
+  DB_HOST
+} = process.env
+
+const url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`
+
+module.exports = mongoose.connect(url, {
+  useNewUrlParser:true,
+  useUnifiedTopology: true
+})
